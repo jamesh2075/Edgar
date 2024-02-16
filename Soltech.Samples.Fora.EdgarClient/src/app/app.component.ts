baf1fc49f9ffe4e9d12ba93286d3526dacb5df44
James Henry <jhenry2075@msn.com>
2024-02-16 06:08:47 -0500

    Added an Angular client

--- Soltech.Samples.Fora.EdgarClient/src/app/app.component.ts
+++ Soltech.Samples.Fora.EdgarClient/src/app/app.component.ts
@@ -1,12 +1,20 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-
+import { environment } from '../environments/environment';
 interface WeatherForecast {
   date: string;
   temperatureC: number;
   temperatureF: number;
   summary: string;
 }
+
+interface Company {
+  id: number;
+  name: string;
+  standardFundableAmount: number;
+  specialFundableAmount: number;
+}
+
 
 @Component({
   selector: 'app-root',
@@ -15,11 +23,15 @@ interface WeatherForecast {
 })
 export class AppComponent implements OnInit {
   public forecasts: WeatherForecast[] = [];
-
+  public companies: Company[] = [];
+  public author: string = '';
+  search: string = '';
   constructor(private http: HttpClient) {}
 
   ngOnInit() {
     this.getForecasts();
+    this.getCompanies();
+    this.getAuthor();
   }
 
   getForecasts() {
@@ -33,5 +45,43 @@ export class AppComponent implements OnInit {
     );
   }
 
-  title = 'angularsample5.client';
+  getCompanies() {
+    this.http.get<Company[]>(`${environment.apiUrl}/api/edgar/companies`).subscribe(
+      (result) => {
+        this.companies = result;
+      },
+      (error) => {
+        console.error(error);
+      }
+    )
+  }
+
+  filter() {
+    if (this.search == undefined || this.search.trim() == '') {
+      this.getCompanies();
+      return;
+    }
+
+    this.http.get<Company[]>(`${environment.apiUrl}/api/edgar/companies/${this.search}`).subscribe(
+      (result) => {
+        this.companies = result;
+      },
+      (error) => {
+        console.error(error);
+      }
+    )
+  }
+
+  getAuthor() {
+    this.http.get(`${environment.apiUrl}/api/edgar/author`, { responseType: 'text' }).subscribe(
+      (result) => {
+        this.author = `Created by: ${result}`;
+      },
+      (error) => {
+        console.error(error);
+      }
+    )
+  }
+
+  title = 'Edgar Company Funding';
 }
