740b9288677462dea6305c91b57b492daeab8b59
James Henry <jhenry2075@msn.com>
2024-02-15 22:14:21 -0500

    Initial Code

---
+++ EdgarForaSampleApi/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Soltech.Samples.Fora.EdgarApi
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
