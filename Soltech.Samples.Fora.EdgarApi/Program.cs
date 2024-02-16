9710b70e0d971569481a186889d390ae292690b8
James Henry <jhenry2075@msn.com>
2024-02-16 05:03:55 -0500

    Implemented CORS

--- Soltech.Samples.Fora.EdgarApi/Program.cs
+++ Soltech.Samples.Fora.EdgarApi/Program.cs
@@ -1,11 +1,32 @@
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("EdgarAPI",
+        policy =>
+        {
+            policy.WithOrigins("https://localhost:4200", "https://edgarclient.azurewebsites.net", "http://localhost:5286").AllowCredentials();
+        });
+});
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen((c) =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = $"Edgar Company Funding API - {builder.Environment.EnvironmentName}", Description = "Serving You the Food You Love", Version = "v1" });
+});
+
 var app = builder.Build();
 
+app.UseSwagger();
+app.UseSwaggerUI();
+
 app.UseAuthorization();
+
+app.UseCors("EdgarAPI");
 
 app.MapControllers();
 
