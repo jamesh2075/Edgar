40bd19b92e329509acaedb645fa9153368c2f736
James Henry <jhenry2075@msn.com>
2024-03-03 15:12:18 -0500

    Changed the base directory for the Vue app so that it runs correctly in Azure.

--- Edgar.Vue/vite.config.ts
+++ Edgar.Vue/vite.config.ts
@@ -86,6 +86,7 @@ export default defineConfig(({ command, mode }) => {
                 key: fs.readFileSync(keyFilePath),
                 cert: fs.readFileSync(certFilePath),
             }
-        }
+        },
+        base: '/Vue/'
     }
 })
